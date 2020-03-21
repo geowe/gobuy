ba04cc25856b41fa95e634f7d64591c0cf57e4d2
Ata <Ata@LEGEND-VR>
2020-03-21 19:16:14 +0100

    Integrados los incrementes y decrementos de clientes, pero comentado hasta la próxima versión

--- src/page/EstablishmentListPage.js
+++ src/page/EstablishmentListPage.js
@@ -1,6 +1,8 @@
 import establishmentListHtml from '../html/establishmentList.html';
 import Page from './Page';
 const URL = 'https://geowe.org/gobuy/service/api.php/records/ESTABLECIMIENTOS?';
+const incrementURL = 'http://geowe.org/gobuy/service/inc_counter.php?';
+const decrementURL = 'http://geowe.org/gobuy/service/dec_counter.php?';
 
 const HOME_BUTTON = `<input id="cancelBtn" type="submit" value="Volver">`;
 
@@ -13,16 +15,16 @@ class EstablishmentListPage extends Page {
 
         const response = await fetch(`${URL}filter=ID_MUNICIPIO,eq,${townId}&filter=ID_CATEGORIA,eq,${categoryId}`);
         const data = await response.json();
-        
+
         this._content.innerHTML = establishmentListHtml.trim();
         const content = document.getElementById("content");
 
         var row = ` <div class="row">`;
         var cont = 0;
         for (let establishment of data.records) {
-                
+
             row = row + this.getEstablishmentCard(establishment);
-            cont++;            
+            cont++;
             if (cont === 4) {
                 row = row + "</div>";
                 content.innerHTML = content.innerHTML + row;
@@ -39,9 +41,9 @@ class EstablishmentListPage extends Page {
         for (let establishment of data.records) {
             var id = establishment.ID_ESTABLECIMIENTO;
 
-            this.registerButtonEvent(`enter_${id}Btn`, this.onEnterClick);
-            this.registerButtonEvent(`leave_${id}Btn`, this.onLeaveClick);
-            this.registerButtonEvent(`map_${id}Btn`, this.onMapClick);
+            this.registerButtonEvent(`enter_${id}Btn`, () => { this.onEnterClick(id); });
+            this.registerButtonEvent(`leave_${id}Btn`, () => { this.onLeaveClick(id); });
+            this.registerButtonEvent(`map_${id}Btn`, () => { this.onMapClick(id); });
         }
     }
 
@@ -52,16 +54,28 @@ class EstablishmentListPage extends Page {
         }
     }
 
-    onEnterClick() {
-        alert("Entramos en el establecimiento con id: " + this.getAttribute("data-id"));
+    onEnterClick(id) {
+        // fetch(`${incrementURL}id=${id}&counter=CONTADOR_CLIENTES_ACTUALES`).
+        // then((response) => {}).
+        // catch((exception) => { alert("Error al incrementar")}) ;
+        // alert("Entramos en el establecimiento con id: " + this.getAttribute("data-id"));
+
+        alert("En desarrollo");
     }
 
-    onLeaveClick() {
-        alert("Salimos del establecimiento con id: " + this.getAttribute("data-id"));
+    onLeaveClick(id) {
+        // fetch(`${decrementURL}id=${id}&counter=CONTADOR_CLIENTES_ACTUALES`).
+        // then((response) => {}).
+        // catch((exception) => { alert("Error al decrementar")}) ;
+        // alert("Salimos del establecimiento con id: " + this.getAttribute("data-id"));
+
+        alert("En desarrollo");
     }
 
-    onMapClick() {
-        alert("Map del establecimiento con id: " + this.getAttribute("data-id") + " coordenadas " + this.getAttribute("data-coordinates"));
+    onMapClick(id) {
+        // alert("Map del establecimiento con id: " + this.getAttribute("data-id") + " coordenadas " + this.getAttribute("data-coordinates"));
+
+        alert("En desarrollo");
     }
 
     getEstablishmentCard(establishment) {
@@ -88,13 +102,13 @@ class EstablishmentListPage extends Page {
             </div>`;
     }
 
-    getPhonesLink(phones){
+    getPhonesLink(phones) {
         let phonesLink = '';
-        if(phones !== null){            
+        if (phones !== null) {
             let phonesSplited = phones.split("-");
-            for (let phone of phonesSplited){
-                let link = `<a href="tel:${phone}">${phone}</a>`;            
-                phonesLink = phonesLink.concat('|'+link+'|');
+            for (let phone of phonesSplited) {
+                let link = `<a href="tel:${phone}">${phone}</a>`;
+                phonesLink = phonesLink.concat('|' + link + '|');
             }
         }
         return phonesLink;
