1e9b2c9bca37da4a91000bb460d93895d74f9271
Rafael López Torres <rafa@geowe.org>
2020-05-14 13:38:29 +0200

    cambios en página de entrada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './page/HomePage';
 
-function onExit() {
+//LO QUITO PARA QUE NO PREGUNTE EN LOS ENLACES
+/*function onExit() {
     return '¿Estás seguro de salir de goBuy?';
 }
 
-window.onbeforeunload = onExit;
+window.onbeforeunload = onExit;*/
